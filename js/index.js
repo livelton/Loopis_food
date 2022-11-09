8551ce10f75aee56b8f1cc61d2ba8c21424aaad1
livelton <elivelton.pereira@academico.ifpb.edu.br>
2022-11-09 10:14:17 -0300

    Criando seleção de utilitários

--- js/index.js
+++ js/index.js
@@ -1,10 +1,4 @@
-let nomePrato = document.querySelector('#nome-prato');
-let precoPrato = document.querySelector('#preco-prato');
-let buttonSave = document.querySelector('#button-save');
-
-buttonSave.addEventListener('click', exibir())
-
-function exibir(){
-    console.log(nomePrato.value, precoPrato.value)
-}
+let dishName = document.querySelector('#name-dish');
+let dishPrice = document.querySelector('#price-dish');
+let saveButton = document.querySelector('#button-save');
 
