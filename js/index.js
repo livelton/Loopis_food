bb63710a3316050f8429ff4e0240a8ce2bb9ed69
livelton <elivelton.pereira@academico.ifpb.edu.br>
2022-11-09 10:17:33 -0300

    Importando register

--- js/index.js
+++ js/index.js
@@ -1,4 +1,9 @@
+import * as productRegistrate from './register-product.js';
+
 let dishName = document.querySelector('#name-dish');
 let dishPrice = document.querySelector('#price-dish');
 let saveButton = document.querySelector('#button-save');
 
+saveButton.addEventListener('click', () => {
+    productRegistrate.registerProduct(dishName.value, dishPrice.value)
+});
