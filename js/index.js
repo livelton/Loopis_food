7103cd968d8b0a623be9f7496d1db29a2dafd275
livelton <elivelton.pereira@academico.ifpb.edu.br>
2022-11-10 10:42:27 -0300

    Consertando o bug da promoção

--- js/index.js
+++ js/index.js
@@ -11,13 +11,12 @@ let cont = 0;
 productsFunctions.showWaring()
 
 saveButton.addEventListener('click', () => {
-
-    productsFunctions.registerProduct(dishName.value, dishPrice.value);
-
     //O aviso será removido somente no primeiro clique do botão
     cont++
     if (cont === 1) {
         productsFunctions.removeWaring();
     }
 
+    //Serão passados os valores digitados para o usuário para a função de registrar produto
+    productsFunctions.registerProduct(dishName.value, dishPrice.value);
 });
