e467f049ea60e72108ba688a2826874ba287bc3e
livelton <elivelton.pereira@academico.ifpb.edu.br>
2022-11-11 22:29:52 -0300

    Separando registra e exibe os produtos em funções

--- js/register-product.js
+++ js/register-product.js
@@ -1,80 +1,92 @@
-import * as sale from './check-sale.js'
-import * as remove from './remove-product.js'
 import * as storage from './storage-item.js'
 
 
 //FUNÇÃO PARA MOSTRAR O AVISO DE NÃO PRODUTO CADASTRADO
-export function showWaring(){
+export function showWaring() {
     let waringNoProduct = document.createElement('h4');
     waringNoProduct.textContent = 'Nenhum produto cadastrado'
-    
+
     let divProduct = document.querySelector('.product-list');
     divProduct.appendChild(waringNoProduct);
-    
+
 }
 
 //FUNÇÃO PARA REMOVER AVISO DE NÃO PRODUTO CADASTRADO
-export function removeWaring(){
+export function removeWaring() {
     let waringNoProduct = document.querySelector('h4');
-    
+
     let divProduct = document.querySelector('.product-list');
     divProduct.removeChild(waringNoProduct);
 }
 
-export function registerProduct(name, price) {
-    let divProduct = document.querySelector('.product-list')
-    
+let dishName = document.querySelector('#name-dish');
+let dishPrice = document.querySelector('#price-dish');
+
+export function registerProduct() {
     let saleYes = document.querySelector('#sale-yes');
-    
-    //Criando a classe e o ID do produto
-    let product = document.createElement('ul')
-    product.setAttribute('class', 'box-product')
-    product.setAttribute('id', 'product')
-    if(saleYes.checked){
-        let firstProduct = divProduct.querySelector("ul");
-        divProduct.insertBefore(product, firstProduct)
-    } else{
-        divProduct.appendChild(product)
-    }
-    
-    if(saleYes.checked){
-        //aqui definirá as propriedades do produto
-        sale.checkSale(product)
+    storage.localStorageItem(dishName.value, dishPrice.value, saleYes.checked);
 
-        //aqui definirá o desconto do produto
-        price = price - (price * 20/100);
-    }
+    updateProduct();
 
-    //Adicionando imagem ao produto
-    let img = document.createElement('img')
-    img.setAttribute('src', '/imgs/hamburguer.png')
-    img.setAttribute('width', '100')
-    img.setAttribute('height', '100')
-    product.appendChild(img);
+}
 
-    //Adicionando informações do produto
-    let infosContentDiv = document.createElement('div')
-    infosContentDiv.setAttribute('class', 'infos-content')
-    product.appendChild(infosContentDiv)
+export function updateProduct() {
+    let pullStorage = storage.recoverLocalStorage();
 
-    let productName = document.createElement('p')
-    let productPrice = document.createElement('p')
-    productName.textContent = `Nome: ${name}`;
-    productPrice.textContent = `Preço: R$${price}`;
-    infosContentDiv.appendChild(productName)
-    infosContentDiv.appendChild(productPrice)
+    let itemsHTML = pullStorage.map((element, index) => {
 
-    //Adicionando botão 
-    let divRemove = document.createElement('div')
-    divRemove.setAttribute('class', 'button-remove')
-    product.appendChild(divRemove)
-    let buttonRemove = document.createElement('button')
-    divRemove.appendChild(buttonRemove)
-    buttonRemove.textContent = 'Remover'
+        //Criando a classe e o ID do produto
+        let product = document.createElement('ul')
+        product.setAttribute('class', 'box-product')
+        product.setAttribute('id', 'product')
 
-    buttonRemove.addEventListener('click', () => {
-        remove.removeProduct(infosContentDiv)
+        console.log()
+        if (element.sale) {
+            product.style.backgroundColor = "#E4B056";
+            //aqui definirá as propriedades do produto
+            //sale.checkSale(product)
+        }
+
+        //Adicionando imagem ao produto
+        let img = document.createElement('img')
+        img.setAttribute('src', '/imgs/hamburguer.png')
+        img.setAttribute('width', '100')
+        img.setAttribute('height', '100')
+
+        //Adicionando informações do produto
+        let infosContentDiv = document.createElement('div')
+        infosContentDiv.setAttribute('class', 'infos-content')
+
+
+        let productName = document.createElement('p')
+        let productPrice = document.createElement('p')
+        productName.textContent = `Nome: ${element.name}`;
+        productPrice.textContent = `Preço: R$${element.price}`;
+
+
+        //Adicionando botão 
+        let divRemove = document.createElement('div')
+        divRemove.setAttribute('class', 'button-remove')
+
+        let buttonRemove = document.createElement('button')
+        buttonRemove.textContent = 'Remover'
+
+        buttonRemove.addEventListener('click', () => {
+            storage.removeProduct(index)
+            updateProduct();
+        })
+
+        divRemove.appendChild(buttonRemove)
+        infosContentDiv.appendChild(productName)
+        infosContentDiv.appendChild(productPrice)
+        product.appendChild(img);
+        product.appendChild(infosContentDiv)
+        product.appendChild(divRemove)
+
+
+        return product;
     })
 
-    storage.localStorageItem(name, price);
+    const divProduct = document.querySelector('.product-list')
+    divProduct.replaceChildren(...itemsHTML);
 }
